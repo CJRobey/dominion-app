85d7688a8b9f5ec506a8dc2ef93ff5c9c5d299c7
cjrobey <calebrobey118@gmail.com>
2020-01-29 07:31:30 -0600

    button working, but not overwriting images, everything is in a column

--- beeware-app/dominionapp/src/dominionapp/app.py
+++ beeware-app/dominionapp/src/dominionapp/app.py
@@ -8,6 +8,9 @@ from toga.style.pack import COLUMN, ROW
 from numpy import zeros, array, sum
 from random import randint, sample
 from pandas import read_csv
+from cv2 import imread
+
+box = toga.Box()
 
 
 class DominionApp(toga.App):
@@ -15,27 +18,14 @@ class DominionApp(toga.App):
     def startup(self):
         self.main_window = toga.MainWindow(title=self.name)
 
-        box = toga.Box()
         box.style.padding = 40
         box.style.update(alignment=CENTER)
         box.style.update(direction=COLUMN)
-
+        button1 = toga.Button('Choose Game', on_press=self.decide_game)
+        box.add(button1)
         # image from local path
         # load brutus.png from the package
         # We set the style width/height parameters for this one
-        image_from_path = toga.Image('./resources/images/abondoned-mine.jpg')
-        print(image_from_path)
-        imageview_from_path = toga.ImageView(image_from_path)
-        imageview_from_path.style.update(height=320)
-        imageview_from_path.style.update(width=200)
-        box.add(imageview_from_path)
-
-        # image from remote URL
-        # no style parameters - we let Pack determine how to allocate
-        # the space
-        #image_from_url = toga.Image('https://beeware.org/project/projects/libraries/toga/advance.jpg')
-        #imageview_from_url = toga.ImageView(image_from_url)
-        #box.add(imageview_from_url)
 
         self.main_window.content = box
         self.main_window.show()
@@ -43,17 +33,20 @@ class DominionApp(toga.App):
     def decide_game(self, widget):
         games = ['Dominion', 'Intrigue']
         total_cards = 10
-        df = read_csv('src/helloworld/resources/characters.csv')
+        df = read_csv('src/dominionapp/resources/characters.csv')
         nums = zeros(5)
         nums[0] = randint(0, 3)
+        # this should be rare that we get 3 "2"s. Guess again, but at least 1
+        if nums[0] == 3:
+            nums[0] = randint(1, 3)
         if nums[0] == 0:
             nums[1] = randint(1, 3)
         else:
             nums[1] = randint(0, 3)
 
         while sum(nums) != total_cards:
-            nums[2] = randint(2, 4)
-            nums[3] = randint(2, 4)
+            nums[2] = randint(2, 5)
+            nums[3] = randint(2, 5)
             nums[4] = randint(0, 2)
         nums = nums.astype(int)
         final_characters = [] * 10
@@ -61,8 +54,20 @@ class DominionApp(toga.App):
             characters = array(df[df['Value'] == (i + 2)]['Name'])
             if num:
                 final_characters[sum(nums[:i]):sum(nums[:i]) + int(num)] = characters[sample(range(0, len(characters)), int(num))]
+
+        for i in range(1,11):
+            image_from_path = toga.Image('./resources/images/' + (final_characters[i-1].lower().replace(" ", "-") + ".jpg"))
+            imageview_from_path = toga.ImageView(image_from_path)
+            imageview_from_path.style.update(width=150)
+            imageview_from_path.style.update(height=240)
+            imageview_from_path.style.update(direction=ROW)
+            box.add(imageview_from_path)
+
         print(final_characters)
         print(nums)
+
+        self.main_window.content = box
+        self.main_window.show()
 
 
 def main():
