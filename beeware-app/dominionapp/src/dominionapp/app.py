486d24ada48a55a261f4f18bc3c98b6b92cf45b4
cjrobey <calebrobey118@gmail.com>
2020-02-08 17:55:48 -0600

    WORKING: path issue resolved

--- beeware-app/dominionapp/src/dominionapp/app.py
+++ beeware-app/dominionapp/src/dominionapp/app.py
@@ -8,33 +8,33 @@ from toga.style.pack import COLUMN, ROW
 from numpy import zeros, array, sum
 from random import randint, sample
 from pandas import read_csv
-from cv2 import imread
-
-box1 = toga.Box()
-box2 = toga.Box()
+import os
+ 
 
 class DominionApp(toga.App):
 
     def startup(self):
-        self.main_window = toga.MainWindow(title=self.name)
-
-        for box in [box1, box2]:
-            box.style.padding = 40
-            box.style.update(alignment=LEFT)
-            box.style.update(direction=ROW)
-        button1 = toga.Button('Choose Game', on_press=self.decide_game)
-        box1.add(button1)
+        self.main_window = toga.MainWindow(title=self.name, size=(250, (240*10)+(40*20)))
+        self.outer_box = toga.Box()
+        self.outer_box.style.padding = 40
+        self.outer_box.style.update(direction=COLUMN)
+        self.button = toga.Button('Choose Game', on_press=self.decide_game, style=Pack(alignment=CENTER, padding=40))
+        self.outer_box.add(self.button)
+        box = toga.Box(style=Pack(padding=40,alignment=CENTER, direction=COLUMN))
+        # initializing the space
+        self.outer_box.add(box)
+        self.scroller = toga.ScrollContainer(content=self.outer_box)
+        #self.scroller.content = self.outer_box
         # image from local path
-        # load brutus.png from the package
         # We set the style width/height parameters for this one
-
-        self.main_window.content = box1
+        self.main_window.content = self.scroller
         self.main_window.show()
+
 
     def decide_game(self, widget):
         games = ['Dominion', 'Intrigue']
         total_cards = 10
-        df = read_csv('src/dominionapp/resources/characters.csv')
+        df = read_csv('app/dominionapp/resources/characters.csv')
         nums = zeros(5)
         nums[0] = randint(0, 3)
         # this should be rare that we get 3 "2"s. Guess again, but at least 1
@@ -50,28 +50,30 @@ class DominionApp(toga.App):
             nums[3] = randint(2, 5)
             nums[4] = randint(0, 2)
         nums = nums.astype(int)
-        final_characters = [] * 10
+        final_characters = [None] * 10
         for i, num in enumerate(nums):
-            characters = array(df[df['Value'] == (i + 2)]['Name'])
+            characters = array(df[df['Cost'] == (i + 2)]['Name'])
             if num:
                 final_characters[sum(nums[:i]):sum(nums[:i]) + int(num)] = characters[sample(range(0, len(characters)), int(num))]
-
+        print(final_characters)
+        picture_box = toga.Box(style=Pack(direction=COLUMN))
         for i in range(1,11):
             image_from_path = toga.Image('./resources/images/' + (final_characters[i-1].lower().replace(" ", "-") + ".jpg"))
-            imageview_from_path = toga.ImageView(image_from_path)
-            imageview_from_path.style.update(width=150, height=240)
-            imageview_from_path.style.update(direction=ROW, alignment=LEFT, padding=24)
-            if (i < 6):
-                box1.add(imageview_from_path)
-            else:
-                box2.add(imageview_from_path)
+            imageview_from_path = toga.ImageView(image_from_path, style=Pack(direction=COLUMN, alignment=CENTER,
+                                                                             padding=40, width=150, height=240))
+            picture_box.add(imageview_from_path)
 
-        print(final_characters)
-        print(nums)
-        box1.add(box2)
-        self.main_window.content = box1
-        self.main_window.show()
+        if len(self.outer_box.children) > 1:
+           self.outer_box.children[1] = picture_box
+        else:
+           self.outer_box.add(picture_box)
+        self.scroller.content = self.outer_box
+        self.main_window.content = self.scroller
 
 
 def main():
-    return DominionApp()
+    return DominionApp('DominionApp')
+
+if __name__ == '__main__':
+    app = main()
+    app.main_loop()
