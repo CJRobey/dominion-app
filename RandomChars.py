508a8f88903369dfd747096899d172741a382452
cjrobey <calebrobey118@gmail.com>
2020-02-17 18:42:08 -0600

    finished pure python implementation

--- RandomChars.py
+++ RandomChars.py
@@ -1,29 +1,24 @@
 import random as rand
-import numpy
-import pandas as pd
-import tinynumpy as np
+import csv
 import matplotlib.pyplot as plt
 
 def main():
     games = ['Dominion', 'Intrigue']
     total_cards = 10
-    # csv_file = 'characters.csv'
+    max_cost = 6
     csv_file = 'cards.csv'
-    full_df = pd.read_csv(csv_file)
+    full_df = [[] for sub in range(max_cost+1)]
+    key_dict = {}
+    csvfile = csv.DictReader(open(csv_file))
+    for row in csvfile:
+        for game in games:
+            if row['Expansion'] == game:
+                cost = int(row['Cost'])
+                print(cost, row['Name'], bool(row['Action']))
+                if bool(row['Action']) or (bool(row['Treasure']) and bool(row['Victory'])):
+                    full_df[cost] += [row]
 
-    if (csv_file == 'cards.csv'):
-        action_bools = (full_df['Action'] == float(1))
-        df = full_df[action_bools]
-        bools = [[],[]]
-        for i,game in enumerate(games):
-            bools[i] = (df['Expansion'] == game)
-        final_bools = np.array(bools[0]) | np.array(bools[1])
-
-        # final_bools = [(bools[0][i] or bools[1][i]) for i in range(len(bools[0]))]
-
-        df = df[final_bools]
-
-    nums = np.zeros(5)
+    nums = [0 for sub in range(5)]
     nums[0] = rand.randint(0, 3)
     # this should be rare that we get 3 "2"s. Guess again, but at least 1
     if nums[0] == 3:
@@ -33,31 +28,30 @@ def main():
     else:
         nums[1] = rand.randint(0, 3)
 
-    while np.sum(nums) != total_cards:
+    while sum(nums) != total_cards:
         nums[2] = rand.randint(2, 5)
         nums[3] = rand.randint(2, 5)
         nums[4] = rand.randint(0, 2)
         if nums[4] == 2:
             nums[4] = rand.randint(0, 2)
 
-    nums = nums.astype(int)
-    final_characters = []*10
+    final_characters = ['filler_name' for i in range(total_cards)]
+    counter = 0;
     for i, num in enumerate(nums):
-        if csv_file == 'cards.csv':
-            char_bools = (df['Cost'] == float(i+2))
-            characters = np.array(df[char_bools]['Name'])
-        else:
-            characters = np.array(df[df['Cost'] == (i+2)]['Name'])
+        characters = full_df[i+2]
         if num:
             sample = rand.sample(range(0, len(characters)), int(num))
-            rng_min = np.sum(nums[:i])
-            rng_max = np.sum(nums[:i])+int(num)
-            final_characters[rng_min:rng_max] = characters[sample]
+            print(num, sample)
+            rng_min = sum(nums[:i])
+            rng_max = sum(nums[:i])+int(num)
+            for s in sample:
+                final_characters[counter] = characters[s]
+                counter += 1
     print(final_characters)
     print(nums)
 
     for i in range(1,11):
-        image = plt.imread('./beeware-app/dominionapp/src/dominionapp/resources/images/' + (final_characters[i-1].lower().replace(" ", "-") + ".jpg"))
+        image = plt.imread('./beeware-app/dominionapp/src/dominionapp/resources/images/' + (final_characters[i-1]['Name'].lower().replace(" ", "-") + ".jpg"))
         plt.subplot(2,5,i); plt.imshow(image); plt.axis("off")
 
     #plt.savefig('final_plot')
--- beeware-app/dominionapp/src/dominionapp/app.py
+++ beeware-app/dominionapp/src/dominionapp/app.py
@@ -5,11 +5,9 @@ import toga
 from toga.style.pack import CENTER, COLUMN, RIGHT, LEFT
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
-import tinynumpy as np
+import csv
 from random import randint, sample
-from pandas import read_csv
-import os
- 
+
 
 class DominionApp(toga.App):
 
@@ -32,21 +30,22 @@ class DominionApp(toga.App):
 
 
     def decide_game(self, widget):
+        # widget.label = 'pressed'
         games = ['Dominion', 'Intrigue']
         total_cards = 10
+        max_cost = 6
         csv_file = 'cards.csv'
-        full_df = read_csv('src/dominionapp/resources/'+csv_file)
+        full_df = [[] for sub in range(max_cost + 1)]
+        key_dict = {}
+        csvfile = csv.DictReader(open('app/dominionapp/resources/' + csv_file))
+        for row in csvfile:
+            for game in games:
+                if row['Expansion'] == game:
+                    cost = int(row['Cost'])
+                    if bool(row['Action']) or (bool(row['Treasure']) and bool(row['Victory'])):
+                        full_df[cost] += [row]
 
-        if csv_file == 'cards.csv':
-            action_bools = (full_df['Action'] == float(1))
-            df = full_df[action_bools]
-            bools = [[], []]
-            for i, game in enumerate(games):
-                bools[i] = (df['Expansion'] == game)
-            final_bools = np.array(bools[0]) | np.array(bools[1])
-            df = df[final_bools]
-
-        nums = []*5
+        nums = [0 for sub in range(5)]
         nums[0] = randint(0, 3)
         # this should be rare that we get 3 "2"s. Guess again, but at least 1
         if nums[0] == 3:
@@ -56,30 +55,28 @@ class DominionApp(toga.App):
         else:
             nums[1] = randint(0, 3)
 
-        while np.sum(nums) != total_cards:
+        while sum(nums) != total_cards:
             nums[2] = randint(2, 5)
             nums[3] = randint(2, 5)
             nums[4] = randint(0, 2)
             if nums[4] == 2:
                 nums[4] = randint(0, 2)
 
-        nums = nums.astype(int)
-        final_characters = [] * total_cards
+        final_characters = ['filler_name' for i in range(total_cards)]
+        counter = 0;
         for i, num in enumerate(nums):
-            if csv_file == 'cards.csv':
-                char_bools = (df['Cost'] == float(i + 2))
-                characters = np.array(df[char_bools]['Name'])
-            else:
-                characters = np.array(df[df['Cost'] == (i + 2)]['Name'])
+            characters = full_df[i + 2]
             if num:
                 char_sample = sample(range(0, len(characters)), int(num))
-                rng_min = sum(nums[:i])
-                rng_max = sum(nums[:i]) + int(num)
-                final_characters[rng_min:rng_max] = characters[char_sample]
+
+                for s in char_sample:
+                    final_characters[counter] = characters[s]
+                    counter += 1
+        print(final_characters)
 
         picture_box = toga.Box(style=Pack(direction=COLUMN))
         for i in range(1,11):
-            image_from_path = toga.Image('./resources/images/' + (final_characters[i-1].lower().replace(" ", "-") + ".jpg"))
+            image_from_path = toga.Image('./resources/images/' + (final_characters[i-1]['Name'].lower().replace(" ", "-") + ".jpg"))
             imageview_from_path = toga.ImageView(image_from_path, style=Pack(direction=COLUMN, alignment=CENTER,
                                                                              padding=40, width=150, height=240))
             picture_box.add(imageview_from_path)
