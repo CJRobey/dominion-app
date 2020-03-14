3a94b5d2d8605ee45f639dd05e6bb2e70f16eb05
cjrobey <calebrobey118@gmail.com>
2020-03-14 15:09:05 -0500

    added balanced and attack games with better code

--- beeware-app/dominionapp/src/dominionapp/app.py
+++ beeware-app/dominionapp/src/dominionapp/app.py
@@ -13,16 +13,22 @@ from random import randint, sample
 class DominionApp(toga.App):
 
     def startup(self):
-        self.main_window = toga.MainWindow(title=self.name)
-        self.outer_box = toga.Box()
-        self.outer_box.style.update(direction=COLUMN)
+        self.main_window = toga.MainWindow(title=self.name) #, ))
+        self.outer_box = toga.Box(style=Pack(flex=1))
+        self.outer_box.style.update(direction=COLUMN, alignment=RIGHT)
 
-        self.choose_button = toga.Button('Choose Game', on_press=self.decide_game, style=Pack(alignment=CENTER, padding=40))
+        self.choose_button = toga.Button('Random Game', on_press=self.decide_game, style=Pack(alignment=CENTER,
+                                                                                              flex=1, padding=10))
+        self.attack_button = toga.Button('Attack Game', on_press=self.decide_attack_game, style=Pack(alignment=CENTER,
+                                                                                              flex=1, padding=10))
+        self.balanced_button = toga.Button('Balanced Game', on_press=self.decide_balanced_game, style=Pack(alignment=CENTER,
+                                                                                              flex=1, padding=10))
         self.outer_box.add(self.choose_button)
-
+        self.outer_box.add(self.attack_button)
+        self.outer_box.add(self.balanced_button)
         # initializing the space
         self.scroller = toga.ScrollContainer(content=self.outer_box)
-        self.init_menu()
+        self.init_games()
         self.split = toga.SplitContainer()
         self.split.content = [self.scroller, self.game_dashboard]
 
@@ -31,7 +37,7 @@ class DominionApp(toga.App):
         self.main_window.content = self.split
         self.main_window.show()
 
-    def init_menu(self):
+    def init_games(self):
         self.games = []
         csv_file = 'cards.csv'
         self.csvfile = csv.DictReader(open('app/dominionapp/resources/' + csv_file))
@@ -40,20 +46,19 @@ class DominionApp(toga.App):
         self.game_dashboard.add(toga.Label('Expansions to Use', style=Pack(text_align=CENTER, padding=10)))
         for row in self.csvfile:
             if row['Expansion'] not in expansion_buttons:
-                button = toga.Button(row['Expansion'], on_press=self.button_callback, style=Pack(alignment=CENTER, padding=5))
+                button = toga.Button(row['Expansion'], on_press=self.button_menu_callback, style=Pack(alignment=CENTER, padding=5))
                 expansion_buttons.append(row['Expansion'])
                 self.game_dashboard.add(button)
 
-    def button_callback(self, widget):
+    def button_menu_callback(self, widget):
         if widget.label[0] != '+':
             self.games += [widget.label]
             widget.label = '+ ' + widget.label
         else:
             self.games.remove(widget.label[2:])
             widget.label = widget.label[2:]
-        print(self.games)
 
-    def decide_game(self, widget):
+    def grab_cards(self):
         max_cost = 6
         full_df = [[] for sub in range(max_cost + 1)]
         total_cards = 10
@@ -63,7 +68,7 @@ class DominionApp(toga.App):
         for row in self.csvfile:
             for game in self.games:
                 if row['Expansion'] == game:
-                    print(row['Cost'], row['Name'])
+                    # print(row['Cost'], row['Name'])
                     if row['Cost'] == '':
                         row['Cost'] = 0
                     cost = int(row['Cost'])
@@ -91,28 +96,57 @@ class DominionApp(toga.App):
         counter = 0;
         for i, num in enumerate(nums):
             characters = full_df[i + 2]
-            # print(len(characters), int(num))
 
             if num:
                 char_sample = sample(range(0, len(characters)), int(num))
                 for s in char_sample:
                     final_characters[counter] = characters[s]
                     counter += 1
-        # print(final_characters)
+        return final_characters
 
+    def display_pics(self, final_characters):
         picture_box = toga.Box(style=Pack(direction=COLUMN))
         for i in range(1,11):
             image_from_path = toga.Image('./resources/images/' + (final_characters[i-1]['Name'].lower().replace(" ", "-") + ".jpg"))
             imageview_from_path = toga.ImageView(image_from_path, style=Pack(direction=COLUMN, alignment=CENTER,
-                                                                             padding=40, width=150, height=240))
+                                                                             padding=40, width=150, height=240, flex=1))
             picture_box.add(imageview_from_path)
 
-        if (len(self.outer_box.children) > 1):
-            self.outer_box.children[1] = picture_box
+        if len(self.outer_box.children) > 3:
+            self.outer_box.children[3] = picture_box
         else:
             self.outer_box.add(picture_box)
 
         self.scroller.content = self.outer_box
+        self.main_window.show()
+
+    async def decide_attack_game(self, widget):
+        attack_count = 0
+        while attack_count < 2:
+            attack_count = 0
+            final_characters = self.grab_cards()
+            for char in final_characters:
+                if char['Attack']:
+                    attack_count += 1
+        self.display_pics(final_characters)
+
+    async def decide_balanced_game(self, widget):
+        attack_count = 0
+        react_count = 0
+        while (attack_count + react_count) < 3 and (attack_count == 0 or react_count == 0):
+            attack_count = 0
+            react_count = 0
+            final_characters = self.grab_cards()
+            for char in final_characters:
+                if char['Attack']:
+                    attack_count += 1
+                elif char['Reaction']:
+                    react_count += 1
+
+        self.display_pics(final_characters)
+
+    def decide_game(self, widget):
+        self.display_pics(self.grab_cards())
 
 
 def main():
